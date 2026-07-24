73d7cd8b4c47a52f98cdc6e4367fe4b205d4b138
MohamedG23 <yourname@email.com>
2026-07-24 10:20:57 +0300

    newwwwww

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import { CiSearch } from "react-icons/ci";
+import { FiMenu } from "react-icons/fi";
 
 export default function Layout() {
     const navgetion= useNavigate()
@@ -15,17 +16,35 @@ export default function Layout() {
         <div className='container flex flex-col gap-3'>
             <div className='flex justify-between items-center'>
                 <h1 className='text-4xl'>KAIRA</h1>
-            <div className='flex gap-5'>
+            <div className='md:flex gap-5 hidden'>
                 <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/">Home</NavLink>
                 <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/Shop">Shop</NavLink>
                 <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/About">About</NavLink>
                 <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/Contact">Contact Us</NavLink>
             </div>
-            <div className='flex gap-3'>
+
+            <div className='dropdown dropdown-end md:hidden'>
+                <div tabIndex={0} role="button" className="btn btn-ghost rounded-field">
+                    <FiMenu className='font-bold text-3xl' />
+                </div>
+                <div tabIndex="-1" className="menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-52 p-2 shadow-sm gap-2 flex flex-col">
+                    <div className='flex gap-5 flex-col'>
+                <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/">Home</NavLink>
+                <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/Shop">Shop</NavLink>
+                <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/About">About</NavLink>
+                <NavLink className={({ isActive }) =>'hover:text-black p-3 rounded ' + ( isActive ? 'text-black' : 'text-gray-500')} to="/Contact">Contact Us</NavLink>
+            </div>
+                <div className='flex gap-3 px-1 '>
+                <NavLink className='text-[12px]' to="/">WISHLIST ( 0 ) </NavLink>
+                <NavLink className='text-[12px]' to="/">CART ( 0 )  </NavLink>
+                <CiSearch className='text-2xl font-bold text-[18px]'/>
+            </div>
+                </div>
+            </div>
+            <div className='md:flex gap-3 hidden'>
                 <NavLink to="/">WISHLIST ( 0 ) </NavLink>
                 <NavLink to="/">CART ( 0 )  </NavLink>
                 <CiSearch className='text-2xl font-bold'/>
-
             </div>
             </div>
             <div><Outlet/></div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='flex flex-col justify-center items-center'>
-        <div className='w-[550px] flex flex-col justify-center items-center gap-6 pt-15'>
-        <h1 className='text-7xl'>New Collections</h1>
+        <div className='w-[320px] md:w-[550px] flex flex-col justify-center items-center gap-6 pt-15'>
+        <h1 className='text-4xl md:text-7xl'>New Collections</h1>
         <p className='w-[90%] text-center text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptas ut dolorum consequuntur, adipisci repellat! Eveniet commodi voluptatem voluptate, eum minima, in suscipit explicabo voluptatibus harum, quibusdam ex repellat eaque!</p>
         </div>
     </div>
